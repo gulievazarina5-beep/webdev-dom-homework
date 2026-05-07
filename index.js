66a0afdc05ce766c680177eb2764aef2fa97500c
gulievazarina99-coder <nnizkovskih83@mail.ru>
2026-05-07 20:35:23 +0300

    подключила API и добавила валидацию

--- index.js
+++ index.js
@@ -5,42 +5,70 @@ const nameInput = document.getElementById('name-input')
 const textInput = document.getElementById('text-input')
 const addButton = document.querySelector('.add-form-button')
 const listElement = document.querySelector('.comments')
+const errorBlock = document.getElementById('error-block')
 
-const comments = [
-    {
-        name: 'Глеб Фокин',
-        date: new Date(),
-        text: 'Это будет первый комментарий на странице',
-        likes: 3,
-        isLiked: false,
-    },
-    {
-        name: 'Варвара.Н',
-        date: new Date(),
-        text: 'Мне нравится как оформлена эта страница! ❤',
-        likes: 75,
-        isLiked: true,
-    },
-]
+let comments = []
 
-renderComments({ comments, listElement, textInputElement: textInput })
+const fetchAndRenderComments = () => {
+    return fetch('https://wedev-api.sky.pro/api/v1/zarina-gulieva/comments', {
+        method: 'GET',
+    })
+        .then((response) => {
+            return response.json()
+        })
+        .then((responseData) => {
+            const appComments = responseData.comments.map((comment) => {
+                return {
+                    name: comment.author.name,
+                    date: new Date(comment.date),
+                    text: comment.text,
+                    likes: comment.likes,
+                    isLiked: false, 
+                }
+            })
+
+            comments = appComments
+            renderComments({
+                comments,
+                listElement,
+                textInputElement: textInput,
+            })
+        })
+}
+
+fetchAndRenderComments()
 
 addButton.addEventListener('click', () => {
-    if (!nameInput.value || !textInput.value) {
-        console.error('Заполните форму')
+    if (!nameInput.value.trim() || !textInput.value.trim()) {
+        errorBlock.textContent = 'Заполните форму, пожалуйста'
+        errorBlock.style.display = 'block'
         return
     }
 
-    comments.push({
-        name: sanitizeHtml(nameInput.value),
-        date: new Date(),
-        text: sanitizeHtml(textInput.value),
-        likes: 0,
-        isLiked: false,
+    errorBlock.style.display = 'none'
+
+    fetch('https://wedev-api.sky.pro/api/v1/zarina-gulieva/comments', {
+        method: 'POST',
+        body: JSON.stringify({
+            name: sanitizeHtml(nameInput.value),
+            text: sanitizeHtml(textInput.value),
+        }),
     })
-
-    renderComments({ comments, listElement, textInputElement: textInput })
-
-    nameInput.value = ''
-    textInput.value = ''
+        .then((response) => {
+            return response.json()
+        })
+        .then(() => {
+            return fetchAndRenderComments()
+        })
+        .then(() => {
+            nameInput.value = ''
+            textInput.value = ''
+        })
 })
+
+const hideError = () => {
+    errorBlock.style.display = 'none'
+}
+
+nameInput.addEventListener('input', hideError)
+textInput.addEventListener('input', hideError)
